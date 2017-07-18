9bb8caee0d8c0b4f84dd963c67d82c2f60c69e59
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-07-18 19:48:57 +0200

    Tweaks

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
-var existFile = require('exists-file')
-var trimNewlines = require('trim-newlines')
-var isDirectory = require('is-directory').sync
+const fs = require('fs')
+const path = require('path')
+const existFile = require('exists-file')
+const trimNewlines = require('trim-newlines')
+const isDirectory = require('is-directory').sync
 
-function getFile (namespace, base, env) {
+const getFile = (namespace, base, env) => {
   env = env ? '.' + env : ''
-  return path.join(base, (namespace + env + '.opts'))
+  return path.join(base, namespace + env + '.opts')
 }
 
-function fileDetected (namespace, filepath, env) {
-  var envFile = getFile(namespace, filepath, env)
-  if (existFile(envFile)) return envFile
-  return null
+const fileDetected = (namespace, filepath, env) => {
+  const envFile = getFile(namespace, filepath, env)
+  return existFile.sync(envFile) ? envFile : null
 }
 
-module.exports = function (namespace) {
-  return function readConfig (yargs) {
-    yargs = yargs || process.argv.slice(2)
-    if (yargs.length !== 1) return yargs
+module.exports = (namespace) => (yargs = process.argv.slice(2)) => {
+  if (yargs.length !== 1) return yargs
 
-    var filepath = path.resolve(yargs[0])
-    if (!isDirectory(filepath)) filepath = path.join(filepath, '..')
+  let filepath = path.resolve(yargs[0])
+  if (!isDirectory(filepath)) filepath = path.join(filepath, '..')
 
-    filepath = fileDetected(namespace, filepath, process.env.NODE_ENV)
-    if (!filepath) return yargs
+  filepath = fileDetected(namespace, filepath, process.env.NODE_ENV)
+  if (!filepath) return yargs
 
-    var config = fs.readFileSync(filepath, 'utf8')
-    config = trimNewlines(config).split('\n')
-
-    return config.concat(yargs)
-  }
+  const config = trimNewlines(fs.readFileSync(filepath, 'utf8')).split('\n')
+  return config.concat(yargs)
 }
--- test/index.js
+++ test/index.js
@@ -1,23 +1,19 @@
-/* global describe, it */
+/* global afterEach, describe, it */
 
 'use strict'
 
-var loadOpts = require('..')('test')
-var path = require('path')
+const loadOpts = require('..')('test')
+const path = require('path')
 
-function cloneArray(arr) {
-  return arr.slice(0)
-}
-
-var originalArgv = cloneArray(process.argv)
+const cloneArray = (arr) => arr.slice(0)
+const originalArgv = cloneArray(process.argv)
 
 describe('load-opts', function () {
-
-  afterEach(function() {
-    process.argv = originalArgv;
+  afterEach(function () {
+    process.argv = originalArgv
   })
 
-  it('without detect file and don\'t pass CLI options, return empty array', function () {
+  it("without detect file and don't pass CLI options, return empty array", function () {
     loadOpts([]).length.should.be.equal(0)
   })
 
@@ -26,18 +22,18 @@ describe('load-opts', function () {
   })
 
   it('passing a file, detect config in the path', function () {
-    var filepath = path.join(__dirname, 'fixture')
+    const filepath = path.join(__dirname, 'fixture')
     loadOpts([filepath]).should.be.eql(['-w 1', filepath])
   })
 
   it('passing file and cli options, choose cli options', function () {
-    var filepath = path.join(__dirname, 'fixture')
+    const filepath = path.join(__dirname, 'fixture')
     loadOpts(['-w 2', filepath]).should.be.eql(['-w 2', filepath])
-  });
+  })
 
   it('load opts files based on env', function () {
-    var filepath = path.join(__dirname, 'fixture')
+    const filepath = path.join(__dirname, 'fixture')
     process.env.NODE_ENV = 'testing'
     loadOpts([filepath]).should.be.eql(['-w 3', filepath])
-  });
+  })
 })
